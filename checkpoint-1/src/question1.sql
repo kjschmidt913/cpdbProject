565b93f05f6b434525cd94a71fc2f2e71c390e31
Zane Denmon <zanedenmon2020@u.northwestern.edu>
2019-10-27 14:42:55 -0500

    updates SQL code.

--- checkpoint-1/src/question1.sql
+++ checkpoint-1/src/question1.sql
@@ -1,3 +1,5 @@
+-- DROP TABLE units, officers_and_units, most_problematic_units;
+
 /* Conenct data_officerhistory to data_policeunit */
 SELECT data_officerhistory.unit_id, data_policeunit.unit_name, data_policeunit.description, data_officerhistory.officer_id
 INTO units
@@ -13,6 +15,9 @@ INNER JOIN units on officer_id = data_officer.id;
 
 /* Calculate average complaint_percentile and filter by avg_complaint_percentile > 75% */
 SELECT unit_id, unit_name, description, AVG(complaint_percentile)
+INTO most_problematic_units
 FROM officers_and_units
 GROUP BY unit_id, unit_name, description
 HAVING AVG(complaint_percentile) > 75;
+
+SELECT * FROM most_problematic_units;
--- checkpoint-1/src/question2.sql
+++ checkpoint-1/src/question2.sql
@@ -1,6 +1,32 @@
-SELECT MAX(salary) as max_salary, unit_id
-FROM (
-SELECT salary, m.officer_id, m.unit_id
-FROM data_salary
-INNER JOIN most_problematic_units_with_officers m on data_salary.officer_id = m.officer_id) as temp
-GROUP BY unit_id;
+-- create a catalog of current officers with their unit and salary
+DROP TABLE salary_analysis, highest_salary, commanders, supervisors;
+
+CREATE TEMP TABLE salary_analysis AS (SELECT o.id officer_id, pu.id unit_id, pu.unit_name, salary
+FROM data_officer o, data_officerhistory oh, data_policeunit pu, data_salary s
+WHERE o.id = oh.officer_id AND oh.unit_id = pu.id AND s.officer_id = o.id
+AND oh.end_date IS NULL AND pu.active AND o.resignation_date IS NULL);
+
+CREATE TEMP TABLE highest_salary AS (
+       SELECT unit_id, max(salary) as max_unit_salary
+       FROM salary_analysis
+       GROUP BY unit_id);
+
+CREATE TEMP TABLE commanders AS (SELECT officer_id, unit_id, unit_name
+FROM salary_analysis s
+WHERE salary = (SELECT max_unit_salary FROM highest_salary hs WHERE hs.unit_id = s.unit_id));
+
+SELECT DISTINCT ON (unit_id) unit_id, unit_name, officer_id
+INTO supervisors
+FROM commanders;
+
+DROP TABLE supervisors_and_complaint_percentiles;
+
+CREATE TEMP TABLE supervisors_and_complaint_percentiles AS (
+    SELECT officer_id, unit_id, unit_name, complaint_percentile
+    FROM supervisors s, data_officer d
+    WHERE s.officer_id = d.id
+);
+
+SELECT s.officer_id, s.unit_id, s.unit_name, s.complaint_percentile
+FROM supervisors_and_complaint_percentiles s, most_problematic_units m
+WHERE s.unit_id = m.unit_id
