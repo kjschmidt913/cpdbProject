9348ed4b6c7bde25b51cda58a6c1aa3b184a6ba7
ascourtas <ascourtas@gmail.com>
2019-10-27 15:07:53 -0500

    update checkpoint 1 question 4

--- checkpoint-1/src/question4.sql
+++ checkpoint-1/src/question4.sql
@@ -1,23 +1,32 @@
--- to be run after previous questions
+-- create a catalog of current officers with their unit and salary
+-- Used for Checkpoint 1 regrade
 
--- get the appropriate allegation data per officer, so we can associate it with the unit
-SELECT num_allegations, num_allegations_disciplined, oau.officer_id, oau.unit_id, oau.description, oau.unit_name
-INTO num_allegations_per_officer
-FROM officers_and_units oau
-INNER JOIN (
-   SELECT COUNT(allegation_id) AS num_allegations, COUNT(CASE WHEN disciplined THEN 1 END) AS num_allegations_disciplined, data_officerallegation.officer_id
-FROM data_officerallegation
-GROUP BY data_officerallegation.officer_id
-    ) temp ON oau.officer_id = temp.officer_id;
+DROP TABLE salary_analysis, highest_salary,commanders;
 
+-- get salary and unit info
+CREATE TABLE salary_analysis AS (SELECT o.id officer_id, o.complaint_percentile complaint_percentile, pu.id unit_id, pu.unit_name, pu.description, salary
+FROM data_officer o, data_officerhistory oh, data_policeunit pu, data_salary s
+WHERE o.id = oh.officer_id AND oh.unit_id = pu.id AND s.officer_id = o.id
+AND oh.end_date IS NULL AND pu.active AND o.resignation_date IS NULL);
 
--- get the average allegations per unit, along with the total allegations per unit and relevant unit information
-SELECT temp.unit_id, temp.unit_name, temp.description, num_allegations, avg_num_allegations
-FROM (
-SELECT AVG(num_allegations) AS avg_num_allegations, unit_id, unit_name, description
-FROM num_allegations_per_officer
-GROUP BY unit_id, unit_name, description
-ORDER BY avg_num_allegations DESC) as temp
-INNER JOIN num_allegations_per_officer temp2 ON temp.unit_id = temp2.unit_id
-GROUP BY temp.unit_id, temp.unit_name, temp.description, avg_num_allegations, num_allegations
-ORDER BY avg_num_allegations DESC;
+-- find the max salary per unit
+CREATE TABLE highest_salary AS (
+       SELECT unit_id, max(salary) as max_unit_salary
+       FROM salary_analysis
+       GROUP BY unit_id);
+
+-- get the officer with the max salary, who is probably the commander
+CREATE TABLE commanders AS (SELECT officer_id, unit_id, unit_name, description, complaint_percentile
+FROM salary_analysis s
+WHERE salary = (SELECT max_unit_salary FROM highest_salary hs WHERE hs.unit_id = s.unit_id));
+
+-- get commanders, their unit info, the number of officer in each unit, the average complaint percentile per unit, and
+-- the commander's complaint percentile
+-- only do this for active officers
+SELECT commanders.unit_id, COUNT(d.officer_id) AS number_of_officers, unit_name, description, commanders.complaint_percentile AS supervisor_complaint_percentile,
+       AVG(o.complaint_percentile) as avg_unit_complaint_percentile
+FROM commanders
+INNER JOIN data_officerhistory d on commanders.unit_id = d.unit_id
+INNER  JOIN data_officer o on d.officer_id = o.id
+WHERE active = 'Yes'
+GROUP BY commanders.unit_id, commanders.unit_name, description, commanders.complaint_percentile;
